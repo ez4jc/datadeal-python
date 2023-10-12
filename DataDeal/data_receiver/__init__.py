13b1b989f5a272b02eb2733c040433936b1baa81
sasdasdsssss <928304668@qq.com>
2023-10-12 14:19:44 +0800

    接收16640字节

--- DataDeal/data_receiver/__init__.py
+++ DataDeal/data_receiver/__init__.py
@@ -15,27 +15,13 @@ def receive_data():
     server_socket.listen(1)
     print(f"Listening on {server_ip}:{server_port}...")
 
-    def parse_double_data(data_str):
-        try:
-            # 分割字符串并将其转换为 double 数值
-            parts = data_str.split()
-            if len(parts) == 3:
-                double_array = [float(parts[0]), float(parts[1]), float(parts[2])]
-                return double_array
-            else:
-                return None
-        except ValueError:
-            return None
-
     # 等待客户端连接
     client_socket, client_address = server_socket.accept()
     print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
 
     # 接收字符串数据
-    data = client_socket.recv(1024).decode("utf-8")  # 接收数据并解码
+    data = client_socket.recv(16640)  # 接收数据
 
-    # 解析字符串数据为 double 数组
-    double_data = parse_double_data(data)
 
     # 关闭客户端连接
     client_socket.close()
@@ -43,4 +29,4 @@ def receive_data():
     # 关闭服务器套接字
     server_socket.close()
 
-    return double_data
+    return data
