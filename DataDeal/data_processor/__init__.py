58f3670fe58a89840b4cf05c6a9f849f9233b3e6
sasdasdsssss <928304668@qq.com>
2023-10-12 14:19:24 +0800

    处理多个点

--- DataDeal/data_processor/__init__.py
+++ DataDeal/data_processor/__init__.py
@@ -2,20 +2,24 @@ import pcl
 import numpy as np
 import math
 
-def convert_to_pointcloud(distance, horizontal_angle, vertical_angle):
-    # 计算 x、y 和 z 坐标
-    horizontal_angle_rad = math.radians(horizontal_angle)
-    vertical_angle_rad = math.radians(vertical_angle)
-    x = distance * math.sin(vertical_angle_rad) * math.cos(horizontal_angle_rad)
-    y = distance * math.sin(vertical_angle_rad) * math.sin(horizontal_angle_rad)
-    z = distance * math.cos(vertical_angle_rad)
+def convert_to_pointcloud(R_dis, L_dis, H_dis):
+    point_clouds = []  # 存储点云对象的列表
 
-    # 创建一个包含一个点的2D numpy 数组
-    point = np.array([[x, y, z]], dtype=np.float32)
+    for i in range(len(R_dis)):
+        # 计算 x、y 和 z 坐标
+        x = R_dis[i]
+        y = L_dis[i]
+        z = H_dis[i]
 
-    # 创建一个 pcl 点云对象
-    cloud = pcl.PointCloud()
-    cloud.from_array(point)
+        # 创建一个包含一个点的2D numpy 数组
+        point = np.array([[x, y, z]], dtype=np.float32)
 
-    # 返回点云对象
-    return cloud
+        # 创建一个 pcl 点云对象
+        cloud = pcl.PointCloud()
+        cloud.from_array(point)
+
+        # 将点云对象添加到列表中
+        point_clouds.append(cloud)
+
+    # 返回点云对象的列表
+    return point_clouds
