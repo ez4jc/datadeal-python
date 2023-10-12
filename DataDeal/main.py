eb276315027e0989423befd2a009f05f1c6988d4
sasdasdsssss <928304668@qq.com>
2023-10-12 14:20:02 +0800

    主函数修改接受点云

--- DataDeal/main.py
+++ DataDeal/main.py
@@ -2,17 +2,84 @@
 from DataDeal.data_receiver import receive_data
 from DataDeal.data_processor import convert_to_pointcloud
 import time
+import struct
+import numpy as np
+
+def bytesToFloat(h1,h2,h3,h4):
+    ba = bytearray()
+    ba.append(h1)
+    ba.append(h2)
+    ba.append(h3)
+    ba.append(h4)
+    return struct.unpack("!f",ba)[0] #把字节转换成浮点数
 
 
 def main():
-    while True:
-        # 接收数据，此处假设接收到的数据是一个包含[距离, 水平角度, 垂直角度]的列表
-        received_data = receive_data()
+    start = 0
+    FrameNum = 16
+    TargetNum = 150
+    data = np.zeros((16, 1024), dtype=np.uint8)
+    data2 = np.zeros(4096, dtype=np.uint8)
 
-        if received_data is not None:
-            distance, horizontal_angle, vertical_angle = received_data
-            point_cloud = convert_to_pointcloud(distance, horizontal_angle, vertical_angle)
-            print(f"第一个点的坐标：{point_cloud[0]}")  # 打印第一个点的坐标
+    R_dis = []
+    L_dis = []
+    H_dis = []
+
+
+    while True:
+        pkt_data = receive_data()
+        packet = list(pkt_data)  # 装进packet，header数据包头部信息，pkt_data数据包数据
+
+        # 判断前12位和数据包总长度，确保数据包接收正确
+        if packet[0] == 0xFF and packet[1] == 0xFF and packet[2] == 0xFF \
+                and packet[3] == 0xFF and packet[4] == 0xFF and packet[5] == 0xFF \
+                and packet[6] == 0x48 and packet[7] == 0x5B and packet[8] == 0x39 \
+                and packet[9] == 0xC2 and packet[10] == 0x7D and packet[11] == 0xF8:
+
+            # 避免错位
+            if packet[14] == 0:
+                for i in range(16):
+                    start = 16 + i * 1040  # 计算当前数据段的起始位置
+                    end = start + 1024  # 计算当前数据段的结束位置
+                    tmp = packet[start:end]  # 提取当前数据段
+                    data[i, 0:1024] = tmp  # 将数据段放入相应的行中
+
+                rxFrameData = np.squeeze(data.reshape(-1, 16 * 1024))
+
+                FrameData1 = rxFrameData[0:FrameNum * 1024:4]
+                FrameData2 = rxFrameData[1:FrameNum * 1024:4]
+                FrameData3 = rxFrameData[2:FrameNum * 1024:4]
+                FrameData4 = rxFrameData[3:FrameNum * 1024:4]
+
+                # 转成浮点数存到data2里
+                for i in range(4096):
+                    data2[i] = bytesToFloat(FrameData4[i], FrameData3[i], FrameData2[i], FrameData1[i])
+
+                # 处理坐标
+                R = np.array(data2[0:TargetNum * 5:5])
+                V = np.array(data2[1:TargetNum * 5:5])
+                P = np.array(data2[2:TargetNum * 5:5])
+                A = np.array(data2[3:TargetNum * 5:5])
+                E = np.array(data2[4:TargetNum * 5:5])
+                P_cal = np.pi * P / 180.0
+                E_cal = np.pi * E / 180.0
+                R_cal = R * np.cos(E_cal)
+                H_cal = R * np.sin(E_cal)
+                R_dis = R_cal * np.cos(P_cal)
+                L_dis = R_cal * np.sin(P_cal)
+                H_dis = H_cal
+
+
+        if packet is not None:
+            point_clouds = convert_to_pointcloud(R_dis, L_dis, H_dis)
+            for cloud in point_clouds:
+                # 将点云对象转换为 numpy 数组
+                points = cloud.to_array()
+
+                # 打印每个点云对象的坐标
+                for point in points:
+                    x, y, z = point
+                    print(f"点云对象坐标：({x}, {y}, {z})")
         else:
             print("等待接收数据...")
 
