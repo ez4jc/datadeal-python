d8482f7e952fef472f105a98f457ef18d3ad82ef
sasdasdsssss <928304668@qq.com>
2023-11-02 10:36:55 +0800

    返回点云文件

--- DataDeal/data_processor/__init__.py
+++ DataDeal/data_processor/__init__.py
@@ -1,18 +1,22 @@
-import open3d as o3d
+import vtkmodules.all as vtk
 
 def convert_to_pointcloud(R_dis, L_dis, H_dis):
-    pcd = o3d.geometry.PointCloud()
-    points = []
+    # 创建一个vtkPoints对象，用于存储点云数据
+    points = vtk.vtkPoints()
+
     for i in range(len(R_dis)):
         # 计算 x、y 和 z 坐标
         x = R_dis[i]
         y = L_dis[i]
         z = H_dis[i]
-        points.append([x, y, z])
 
-    pcd.points = o3d.utility.Vector3dVector(points)
+        # 将点添加到vtkPoints对象
+        points.InsertNextPoint(x, y, z)
 
-    # 返回点云对象的列表
-    return pcd
+    # 创建一个vtkPolyData对象，并将vtkPoints添加到其中
+    point_cloud = vtk.vtkPolyData()
+    point_cloud.SetPoints(points)
+
+    return point_cloud
 
 
